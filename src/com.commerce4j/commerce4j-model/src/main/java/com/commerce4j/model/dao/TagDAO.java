33f61e1a0a4647102c686501cf05a6bd35d1c040
carlos.quijano <carlos.quijano@f1a58f91-dc3a-9d2a-e1c2-6196a763e53b>
2010-05-14 23:03:10 +0000

    Initial Item's Tag Support

---
+++ src/com.commerce4j/commerce4j-model/src/main/java/com/commerce4j/model/dao/TagDAO.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2010 Commerce4J.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.commerce4j.model.dao;
+
+import java.util.List;
+
+import com.commerce4j.model.dto.TagDTO;
+
+/**
+ * @author carlos.quijano
+ * @version $Revision$ $Date$
+ */
+public interface TagDAO {
+	
+	/**
+	 * Find all tags sorted by name for a particular Item. 
+	 * 
+	 * @param itemId The item primary key.
+	 * @return A {@link TagDTO} entity list or <code>null</code> if empty. 
+	 */
+	public List<TagDTO> findAllTagsByItem(Integer itemId);
+}
---
+++ src/com.commerce4j/commerce4j-model/src/main/java/com/commerce4j/model/dao/jdbc/TagDAOImpl.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2010 Commerce4J.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.commerce4j.model.dao.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.commerce4j.model.dao.TagDAO;
+import com.commerce4j.model.dto.TagDTO;
+
+/**
+ * @author carlos.quijano
+ * @version $Revision$ $Date$
+ */
+public class TagDAOImpl extends JdbcDaoSupport implements TagDAO {
+	
+	private RowMapper rowMapper;
+
+
+	/**
+	 * Constructor, Creates a new type instance of {@link ItemDAOImpl}.
+	 */
+	public TagDAOImpl() {
+		super();
+		rowMapper = new TagMapper();
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.commerce4j.model.dao.TagDAO#findAllTagsByItem(java.lang.Integer)
+	 */
+	@SuppressWarnings("unchecked")
+	public List<TagDTO> findAllTagsByItem(Integer itemId) {
+		
+		String sql = "SELECT it.item_id, tag.tag FROM c4j_items_tags tag " +
+				"INNER JOIN c4j_items it ON it.item_id = tag.item_id " +
+				"WHERE it.item_id = ?";
+		
+		Integer[] params = {itemId};
+		return getJdbcTemplate().query(sql, params, rowMapper);
+	}
+	
+	class TagMapper implements RowMapper {
+
+		/* (non-Javadoc)
+		 * @see org.springframework.jdbc.core.RowMapper#mapRow(java.sql.ResultSet, int)
+		 */
+		public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+			TagDTO dto = new TagDTO();
+			dto.setTag(rs.getString("tag"));
+			return dto;
+		}
+		
+	}
+}
